98849322c475343027bd23c76eec5b2b25d1819c
Cindy Salinas <cindysalinas15@gmail.com>
2016-09-05 14:55:36 -0400

    init project

---
+++ app/app.modules.js
@@ -0,0 +1,7 @@
+
+'use strict';
+
+angular.module('rokk3rlabsApp', [
+  'ui.router',
+  'chart.js'
+  ]);
---
+++ app/app.routes.js
@@ -0,0 +1,28 @@
+
+'use strict';
+/**
+ * Load states for application (UI-Router)
+ */
+angular.module('rokk3rlabsApp')
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
+
+    // any unknown URLS go to 404
+    $urlRouterProvider.otherwise('/404');
+    // no route goes to index
+    $urlRouterProvider.when('', '/');
+
+    // use a state provider for routing
+    $stateProvider
+      .state('app', {
+        abstract: true,
+        templateUrl: "./views/home/homeView.html",
+      })
+      .state('app.home', {
+          url: '/',
+          templateUrl: './views/dashboard/dashboardView.html'
+      })
+      .state('404', {
+          url: '/404',
+          templateUrl: './views/not_found/404.html'
+      })
+}]);
